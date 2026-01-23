cbc83ce638c4630893c6c460619fb944339d3658
SamuelHarris09 <samharris2009@hotmail.com>
2026-01-23 16:02:07 +0100

    Added the Player and movescript

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float jumpHeight = 1f;
+    [SerializeField] float jumpDistance = 2f;
+    [SerializeField] LayerMask layerMask;
+
+    bool canControlPlayer = true;
+
+    Vector2 moveVector;
+    Rigidbody2D rb;
+    InputAction moveAction;
+    InputAction jumpAction;
+
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Player/Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (canControlPlayer == true)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
+
+        rb.linearVelocityX = moveVector.x * moveSpeed;
+
+       
+        RaycastHit2D ground = Physics2D.Raycast(transform.position, Vector2.down, jumpDistance, layerMask);
+
+        if (jumpAction.IsPressed() && ground.collider)
+        {
+            rb.linearVelocityY = jumpHeight;
+        }
+    }
+}
