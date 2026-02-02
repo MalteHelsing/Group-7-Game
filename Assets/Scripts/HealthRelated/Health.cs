43c6cc30e2a92089e482b25d6e67e6c18e8bcef0
MalteHelsing <Helsingmalte@gmail.com>
2026-02-02 15:06:33 +0100

    remove things that we dont need

--- Assets/Scripts/HealthRelated/Health.cs
+++ Assets/Scripts/HealthRelated/Health.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] float currentHealth = 10f;
-    [SerializeField] float maxHealth = 10f;
     [SerializeField] float damage = 2f;
 
     private void OnTriggerEnter2D(Collider2D collision)
