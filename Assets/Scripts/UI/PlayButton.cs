0048d3af2aecef24a4c148466d51485f6d9846f3
SamuelHarris09 <samharris2009@hotmail.com>
2026-02-19 16:57:48 +0100

    Coyote Time
    Added Coyote time to the playermovement script.

--- Assets/Scripts/UI/PlayButton.cs
+++ Assets/Scripts/UI/PlayButton.cs
@@ -5,7 +5,6 @@ public class PlayButton : MonoBehaviour
 {
     public void PlayGame()
     {
-        //SceneManager.LoadSceneAsync(1);
-        Debug.Log("Play");
+        SceneManager.LoadSceneAsync(1);
     }
 }
