c5afaf5ca086e8d42f3c87a3ccffb5b709a6cea5
SamuelHarris09 <samharris2009@hotmail.com>
2026-02-20 15:43:48 +0100

    u

--- Assets/Scripts/Health/TestHealth.cs
+++ Assets/Scripts/Health/TestHealth.cs
@@ -8,26 +8,16 @@ public class TestHealth : MonoBehaviour
     [SerializeField] int health = 50;
 
     Slider healthSlider;
-<<<<<<< HEAD
-=======
-
-    InputAction jumpAction;
-    
->>>>>>> db6d51574be3b5f3bf83c4b0d190dafe35320c9a
 
     void Start()
     {
         healthSlider.value = health;
         healthSlider = GetComponent<Slider>();
-        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     void Update()
     {
-       if (jumpAction.IsPressed())
-       {
-            HealthSlider();
-       }
+      
     }
 
     void HealthSlider()
