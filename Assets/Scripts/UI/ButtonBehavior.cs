3e8ca029c6b832b06a0037cb7cde9257f61ecbfd
SamuelHarris09 <samharris2009@hotmail.com>
2026-02-22 16:58:52 +0100

    Redid StartMenu & Added Controls In Options

---
+++ Assets/Scripts/UI/ButtonBehavior.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void StartMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
