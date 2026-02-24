b172ea6868fb9ceac92fec456e4ef21f66caed9d
pontus.ng2025 <pontus.nylundgustafsson@elev.ga.lbs.se>
2026-02-24 10:06:58 +0100

    health bar

--- Assets/Scripts/Health/TestHealth.cs
+++ Assets/Scripts/Health/TestHealth.cs
@@ -6,21 +6,34 @@ public class TestHealth : MonoBehaviour
 {
     [SerializeField] int health = 50;
 
+    InputAction damageAction;
+
     Slider healthSlider;
 
     void Start()
     {
+        damageAction = InputSystem.actions.FindAction("Crouch");
+        
         healthSlider.value = health;
         healthSlider = GetComponent<Slider>();
     }
 
     void Update()
     {
-      
+      if (damageAction.IsPressed())
+        {
+            HealthSlider();
+        }
     }
 
     void HealthSlider()
     {
         healthSlider.value -= 10f;
+    }
+
+    public void SetHealth (float currentHealth, float maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
     }
 }
