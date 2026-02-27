f46d865513b68755edcc56926d6ffc1632afeeea
MalteHelsing <Helsingmalte@gmail.com>
2026-02-27 14:19:51 +0100

    goomba

---
+++ Assets/Scripts/Health/EnemyHealth.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float currentHealth = 10f;
+    [SerializeField] bool isEnemy = false;
+
+    DamageDealer damageDealer;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.GetComponent<DamageDealer>();
+
+        if (damageDealer != null)
+        {
+            if (isEnemy != false)
+            {
+                int layerIndexC = LayerMask.NameToLayer("HitBox");
+
+                if (other.gameObject.layer == layerIndexC)
+                {
+                    TakeDamage(damageDealer.DoDamage());
+                }
+            }
+        }
+
+        Die();
+    }
+
+    public void Die()
+    {
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+}
