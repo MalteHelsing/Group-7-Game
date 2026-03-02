67cec06c255ede3d797ce42cc27c77ccc5647314
SamuelHarris09 <samharris2009@hotmail.com>
2026-03-02 15:10:44 +0100

    Development of Pause Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,28 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timeText;
-    
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
+
     private float timeElapsed = 0f;
+
+    InputAction pauseMenu;
     
     void Start()
     {
-        
+        pauseMenu = InputSystem.actions.FindAction("Pause Menu");
     }
 
     
     void Update()
     {
         TimeCounter();
+        Menu();
     }
 
     void TimeCounter()
@@ -26,4 +32,25 @@ public class GameManager : MonoBehaviour
         int seconds = Mathf.FloorToInt(timeElapsed % 60);
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     } 
+
+    void Menu()
+    {
+        if (pauseMenu.IsPressed())
+        {
+            if (pauseScreen.activeInHierarchy)
+            {
+                PausGame(false);
+            }
+            else
+            {
+                PausGame(true);
+            }
+        }
+    }
+
+    public void PausGame(bool staus)
+    {
+
+        pauseScreen.SetActive(staus);
+    }
 }
