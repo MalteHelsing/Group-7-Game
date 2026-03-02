43bebbcaff6c3326651ce18c064f84048ba3eba2
MalteHelsing <Helsingmalte@gmail.com>
2026-03-02 12:44:25 +0100

    Key script

---
+++ Assets/Scripts/Player/KeyForDoor.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class KeyForDoor : MonoBehaviour
+{
+    [Header("Key")]
+    [SerializeField] float KeyPickUpDelay = 0.1f;
+
+    bool HasKey = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Key") && !HasKey)
+        {
+            HasKey = true;
+            Destroy(other.gameObject, KeyPickUpDelay);
+        }
+        else if (other.CompareTag("Door") && HasKey)
+        {
+            HasKey = false;
+        }
+    }
+}
