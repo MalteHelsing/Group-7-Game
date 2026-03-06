1e46d022f7416c26e53728a613aced28f4996bae
SamuelHarris09 <samharris2009@hotmail.com>
2026-03-06 09:33:03 +0100

    Pause Menu with Arrow Done

--- Assets/Scripts/UI/SelectionArrow.cs
+++ Assets/Scripts/UI/SelectionArrow.cs
@@ -3,29 +3,44 @@ using UnityEngine.InputSystem;
 
 public class SelectionArrow : MonoBehaviour
 {
-    [SerializeField] private RectTransform[] options; 
+    [SerializeField] private RectTransform[] options;
+
+    [Header("Music & SFX")]
+    [SerializeField] AudioClip changeSound;
+    [SerializeField, Range(0, 1)] float changeVolume;
+    
+    [SerializeField] AudioClip interactSound;
+    [SerializeField, Range(0, 1)] float interactVolume;
+
     private RectTransform rect;
     private int currentPosition;
 
-    InputAction moveAction;
-
     private void Update()
     {
-        if (moveAction.IsPressed())
+        if (Keyboard.current.wKey.wasPressedThisFrame || Keyboard.current.upArrowKey.wasPressedThisFrame)
         {
-
+            ChangePosition(-1);
+        }
+        
+        if (Keyboard.current.sKey.wasPressedThisFrame || Keyboard.current.downArrowKey.wasPressedThisFrame)
+        {
+            ChangePosition(+1);
         }
     }
 
     private void Awake()
     {
         rect = GetComponent<RectTransform>();
-        moveAction = InputSystem.actions.FindAction("Player/Move");
     }
 
     private void ChangePosition(int _change)
     {
         currentPosition += _change;
+
+        if(_change != 0)
+        {
+            //AudioSource.PlayClipAtPoint(changeSound, transform.position, changeVolume);
+        }
 
         if (currentPosition < 0)
         {
