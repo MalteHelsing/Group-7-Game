709b4896cc93d6c102b34795eae69eba73c9498e
MalteHelsing <Helsingmalte@gmail.com>
2026-03-06 15:59:42 +0100

    nothing

--- Assets/Scripts/Player/SpearLooker.cs
+++ Assets/Scripts/Player/SpearLooker.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class SpearLooker : MonoBehaviour
 {
     public void SpearLookLeft()
     {
-        transform.position = transform.position + new Vector3(-2, 0, 0);
+        transform.position += new Vector3(-3, 0, 0);
     }
 
     public void SpearLookRight()
     {
-        transform.position = transform.position + new Vector3(2, 0, 0);
+        transform.position += new Vector3(3, 0, 0);
     }
 }
