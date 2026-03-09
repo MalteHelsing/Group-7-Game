855ae94be85e27800a60cb5b3972cbd366882f32
Noocon-3rd <orrinbrandulv@gmail.com>
2026-03-09 12:46:57 +0100

    SkeleBoner

---
+++ Assets/Scripts/Enemy/SkeletonAI.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class SkeletonAI : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float agroRange = 5f;
+    [SerializeField] float runRange = 2.5f;
+
+    private void Update()
+    {
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distToPlayer < runRange)
+        {
+            // Start running like a fucking coward, little baby, little fucking wah wah baby coward.
+            Run();
+        }
+        if (distToPlayer < agroRange)
+        {
+            //SHOOT THAT MOTHERFUCKER!!!!'
+            Aggro();
+        }
+    }
+
+    void Run()
+    {
+        transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+    }
+
+    void Aggro()
+    {
+        //rotate towards player
+        if (transform.position.x < player.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    //welcome to the underground!
+    //how was the fall?
+    //if you want to look around,
+    //give us a call.
+    //
+}
