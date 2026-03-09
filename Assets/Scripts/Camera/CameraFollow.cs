81f70fb438eca88eeca80b32715e8923002125f3
MattisStolpe <stolpemattis@gmail.com>
2026-03-09 13:24:28 +0100

    ACTUALLY added the smoothing fix when sKey is pressed

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -18,6 +18,11 @@ public class CameraFollow : MonoBehaviour
         FindTargetPosition();
     }
 
+    private void Update()
+    {
+        FollowFaster();
+    }
+
     void LateUpdate()
     {
         FindTargetPosition();
