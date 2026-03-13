76e9cab1e97caa739a59b61872cc05871c6143a6
MalteHelsing <Helsingmalte@gmail.com>
2026-03-13 14:23:10 +0100

    SPEAR LOOKER WORKING

--- Assets/Scripts/Player/SpearLooker.cs
+++ Assets/Scripts/Player/SpearLooker.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 public class SpearLooker : MonoBehaviour
 {
+    private Camera mainCam;
+    private Vector3 mousePos;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
     private void Update()
     {
-       
+        mousePos = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
     }
 }
