331f6d871e8804faf7c9a0325f772b5d6d8137b2
MalteHelsing <Helsingmalte@gmail.com>
2026-03-13 13:37:25 +0100

    Update SpearLooker.cs

--- Assets/Scripts/Player/SpearLooker.cs
+++ Assets/Scripts/Player/SpearLooker.cs
@@ -1,19 +1,8 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
-using UnityEngine.XR;
 public class SpearLooker : MonoBehaviour
 {
-    [SerializeField] float maxSpeed = 1f;
-
-    InputAction fireAction;
     private void Update()
     {
-        Vector3 position = transform.position;
-        Vector3 direction;
-        transform.Translate(Vector3.up * maxSpeed * Time.deltaTime * inputDevice.LeftStickY, Space.World);
-        transform.Translate(Vector3.right * maxSpeed * Time.deltaTime * , Space.World);
-        direction = transform.position - position;
+       
     }
 }
