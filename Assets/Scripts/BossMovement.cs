4a613311dabb5c14aab2b847f75770e6802482c9
SamuelHarris09 <samharris2009@hotmail.com>
2026-03-16 14:11:57 +0100

    y

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -4,13 +4,14 @@ public class BossMovement : MonoBehaviour
 {
     Transform[] waypoints;
     int waypointIndex = 0;
+    float w;
 
     void FollowPath()
     {
         if (waypointIndex < waypoints.Length)
         {
             Vector3 targetPostion = waypoints[waypointIndex].position;
-            float moveDelta = waveConfig.GetEnemyMoveSpeed() * Time.deltaTime;
+            float moveDelta = w * Time.deltaTime;
 
             transform.position = Vector2.MoveTowards(transform.position, targetPostion, moveDelta);
 
