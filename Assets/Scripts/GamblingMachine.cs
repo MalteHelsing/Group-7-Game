d593eaad773ee229ae46537550813eacce0c3d7a
pontus.ng2025 <pontus.nylundgustafsson@elev.ga.lbs.se>
2026-04-24 15:50:50 +0200

    Gambling

--- Assets/Scripts/GamblingMachine.cs
+++ Assets/Scripts/GamblingMachine.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GamblingMachine : MonoBehaviour
@@ -5,21 +7,46 @@ public class GamblingMachine : MonoBehaviour
     private Sprite[] gamblingSlots;
     private bool coroutineAllowed = true;
     private SpriteRenderer rend;
-    
+
     private void Start()
     {
         rend = GetComponent<SpriteRenderer>();
         gamblingSlots = Resources.LoadAll<Sprite>("GamblingSlots/");
         rend.sprite = gamblingSlots[100];
     }
-    
+
     private void OnMouseDown()
     {
-       
+        
     }
 
-    
-    
+    private IEnumerator RollTheSlots()
+    {
+        coroutineAllowed = false;
+        int randomGamblingSlots = 0;
+        for (int i = 0; i <= 100; i++)
+        {
+            randomGamblingSlots = Random.Range(0, 100);
+            rend.sprite = gamblingSlots[randomGamblingSlots];
+            yield return new WaitForSeconds(0.05f);
+        } 
+    }
 
-    
+       
+       
+
+        // sprite animation 
+        private SpriteRenderer spriteRenderer;
+
+        public Sprite[] sprites;
+
+        private int spriteIndex;
+
+        private void Awake()
+        {
+         spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+   
 }
+
