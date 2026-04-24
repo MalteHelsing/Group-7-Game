525c617ca03aeb69ffec36f45f94fc5894aca90e
MalteHelsing <Helsingmalte@gmail.com>
2026-04-24 13:31:55 +0200

    test on delay

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -56,6 +56,10 @@ public class PlayerAttack : MonoBehaviour
                 spear.SetActive(!isActive);
                 StartCoroutine(DelayAction(SpearDeActiveDelay));
             }
+            else
+            {
+                spear.SetActive(isActive == false);
+            }
         }
     }
 
