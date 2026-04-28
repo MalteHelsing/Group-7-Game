76fcf3f4298e06e7e672fd4b5e149e0234aaef64
Noocon-3rd <orrinbrandulv@gmail.com>
2026-04-28 08:24:26 +0200

    fdgnio bat

--- Assets/Scripts/Enemy/Bat/BatAttack.cs
+++ Assets/Scripts/Enemy/Bat/BatAttack.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class BatAtatck : MonoBehaviour
     [SerializeField] Transform player;
     [SerializeField] Transform[] attackWayPoints;
 
+    [SerializeField] float attackSpeed = 5f;
     [SerializeField] float iGAYRange = 5f;
     int attackPointIndex = 0;
 
@@ -18,16 +20,35 @@ public class BatAtatck : MonoBehaviour
 
     void Update()
     {
+        batAttackCheck();
+
+        Task.Delay(5);
+    }
+
+    void batAttackCheck()
+    {
         float distToPlayer = Vector2.Distance(transform.position, player.position);
 
-        if (distToPlayer < iGAYRange)
+        if (distToPlayer < iGAYRange && distToPlayer > iGAYRange - 1)
         {
             //graah Im Gonna Attack You graaaaahhhh
             if (attackPointIndex < attackWayPoints.Length)
             {
                 //now i gon aa ttack y uo
-                
+                Vector3 targetPosition = attackWayPoints[attackPointIndex].position;
+                float moveDelta = attackSpeed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveDelta);
+
+                if (transform.position == targetPosition)
+                {
+                    attackPointIndex++;
+                }
+                if (attackPointIndex == attackWayPoints.Length)
+                {
+                    attackPointIndex = 0;
+                }
             }
         }
     }
+
 }
