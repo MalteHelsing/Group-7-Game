2b151d853ea46d43ee7a955d3ff45eb1ca387317
SamuelHarris09 <samharris2009@hotmail.com>
2026-05-04 12:10:47 +0200

    Added Images

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,21 +2,4 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 public class Test : MonoBehaviour
 {
-    [System.Serializable]
-    public class SlotSymbol
-    {
-        public Sprite sprites;
-        public int value;
-    }
-
-    [SerializeFeild] public SlotSymbol[] symbols;
-
-    void Spin()
-    {
-        SlotSymbol s1 = symbols[Random.Range(0, symbols.Length)];
-        SlotSymbol s2 = symbols[Random.Range(0, symbols.Length)];
-        SlotSymbol s3 = symbols[Random.Range(0, symbols.Length)];
-
-        int totalValue = s1.value + s2.value + s3.value;
-    }
 }
